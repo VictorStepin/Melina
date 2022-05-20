1258229f5668ed0fe132e664b3ccb155a1533efb
Victor Stepin <vicstep1994@gmail.com>
2022-05-21 00:19:14 +0700

    WordCard class has been added.

--- Melina/Program.cs
+++ Melina/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿WordCard wordCard = new WordCard("consistent", "последовательный");
+
+Console.WriteLine(wordCard.Word + " " + wordCard.Translation);
---
+++ Melina/WordCard.cs
@@ -0,0 +1,11 @@
+﻿internal class WordCard
+{
+    public string Word { get; }
+    public string Translation { get; }
+
+    public WordCard(string word, string translation)
+    {
+        Word = word;
+        Translation = translation;
+    }
+}
