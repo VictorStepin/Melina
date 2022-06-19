60c9d654ac5d944fd62833a456c7ed5197d7b0ac
Victor Stepin <vicstep1994@gmail.com>
2022-06-19 11:23:18 +0700

    Overhaul CLI

--- Melina/Program.cs
+++ Melina/Program.cs
@@ -1,58 +1,230 @@
-﻿var isProgramRunning = true;
-var wordCards = new List<WordCard>();
+﻿var wordCards = new List<WordCard>();
 LoadWordCardsFromFile("words.txt");
 
-Console.WriteLine("1 - Print all word cards");
-Console.WriteLine("2 - Add new word card");
-Console.WriteLine("3 - Add word cards from file");
-Console.WriteLine("4 - Delete word card");
-Console.WriteLine("x - Exit");
-
+var isProgramRunning = true;
 while (isProgramRunning)
 {
+    UpdateMainMenu();
+    
     var input = Console.ReadKey();
-
-    Console.Clear();
 
     switch (input.Key)
     {
         case ConsoleKey.D1:
-            PrintWordCards();
+            RunWordCardsManagementMenu();
             break;
         case ConsoleKey.D2:
-            Console.WriteLine("Enter a word/phrase: ");
-            var word = Console.ReadLine();
-
-            Console.WriteLine("Enter a translation: ");
-            var translation = Console.ReadLine();
-
-            AddNewWordCard(word, translation);
-            SaveWordCardsToFile("words.txt");
-            
-            Console.WriteLine("The word card has been successfully added.");
-            break;
-        case ConsoleKey.D3:
-            Console.Write("Enter a path: ");
-            var path = Console.ReadLine();
-            
-            LoadWordCardsFromFile(path);
-            SaveWordCardsToFile("words.txt");
-            
-            Console.Write("The new word cards have been successfully added.");
-            break;
-        case ConsoleKey.D4:
-            Console.Write("Enter a word to delete: ");
-            var wordToDelete = Console.ReadLine();
-            
-            DeleteWordCard(wordToDelete);
-            SaveWordCardsToFile("words.txt");
-            
-            Console.WriteLine("The word card has been successfully deleted.");
+            RunTestingMenu();
             break;
         case ConsoleKey.X:
+            Console.Clear();
             isProgramRunning = false;
             break;
+        default:
+            UpdateMainMenu();
+            break;
     }
+}
+
+void UpdateMainMenu()
+{
+    Console.Clear();
+
+    Console.WriteLine("MELINA");
+    Console.WriteLine();
+
+    Console.WriteLine("1 - Word Cards Management");
+    Console.WriteLine("2 - Testing");
+    Console.WriteLine("X - Exit");
+}
+
+void RunWordCardsManagementMenu()
+{
+    var isWordCardsMenuActive = true;
+    var pointerPosition = wordCards.Count - 1;
+
+    while (isWordCardsMenuActive)
+    {
+        UpdateWordCardsManagementMenu(pointerPosition);
+
+        var input = Console.ReadKey();
+
+        switch (input.Key)
+        {
+            case ConsoleKey.DownArrow:
+                if (pointerPosition < wordCards.Count - 1)
+                {
+                    pointerPosition++;
+                }
+                break;
+            case ConsoleKey.UpArrow:
+                if (pointerPosition > 0)
+                {
+                    pointerPosition--;
+                }
+                break;
+            case ConsoleKey.D1:
+                Console.Clear();
+                
+                Console.Write("Enter a word: ");
+                var word = Console.ReadLine();
+
+                Console.Write("Enter a translation: ");
+                var translation = Console.ReadLine();
+
+                Console.Write("Confirm adding (Y/N): ");
+                var confirmAddingInput = Console.ReadKey();
+
+                if (confirmAddingInput.Key == ConsoleKey.Y)
+                {
+                    AddNewWordCard(word, translation);
+                    SaveWordCardsToFile("words.txt");
+                }
+
+                pointerPosition = wordCards.Count - 1;
+                break;
+            case ConsoleKey.D2:
+                var wordCardToEdit = wordCards[pointerPosition];
+
+
+                Console.Clear();
+
+                Console.WriteLine($"Editing {wordCardToEdit.Word} {wordCardToEdit.Translation}");
+
+                Console.Write("Enter a new word: ");
+                var newWord = Console.ReadLine();
+
+                Console.Write("Enter a new Translation: ");
+                var newTranslation = Console.ReadLine();
+
+                Console.Write("Confirm editing (Y/N): ");
+                var confirmEditingInput = Console.ReadKey();
+
+                if (confirmEditingInput.Key == ConsoleKey.Y)
+                {
+                    EditWordCard(wordCardToEdit, newWord, newTranslation);
+                    SaveWordCardsToFile("words.txt");
+                }
+
+                pointerPosition = wordCards.Count - 1;
+                break;
+            case ConsoleKey.D3:
+                var wordCardToDelete = wordCards[pointerPosition];
+
+                Console.Clear();
+
+                Console.Write($"Confirm deleting \"{wordCardToDelete.Word}\" (Y/N): ");
+                var confirmDeletingInput = Console.ReadKey();
+
+                if (confirmDeletingInput.Key == ConsoleKey.Y)
+                {
+                    DeleteWordCard(wordCardToDelete);
+                    SaveWordCardsToFile("words.txt");
+                }
+
+                pointerPosition = wordCards.Count - 1;
+                break;
+            case ConsoleKey.Backspace:
+                isWordCardsMenuActive = false;
+                break;
+            default:
+                UpdateWordCardsManagementMenu(pointerPosition);
+                break;
+        }
+    }
+}
+
+void UpdateWordCardsManagementMenu(int pointerPosition)
+{
+    Console.Clear();
+
+    Console.WriteLine("WORD CARDS");
+    Console.WriteLine();
+
+    string border = "+--------------------------------------------------------------------------------+";
+
+    // Рисуем верхнюю границу
+    Console.WriteLine(border);
+
+    // Рисуем строки области карточек
+    var wcAreaLinesCount = 20;
+    for (int i = 0; i < wcAreaLinesCount; i++)
+    {
+        var content = "|";
+
+        // Переключение указателя по строкам
+        if (pointerPosition < wcAreaLinesCount - 1)
+        {
+            if (i == pointerPosition) content += "->";
+            else content += "  ";
+        }
+        else
+        {
+            if (i == wcAreaLinesCount - 1) content += "->";
+            else content += "  ";
+        }
+
+        // Заполнение содержательной частью
+        WordCard wordCard;
+        if (pointerPosition < wcAreaLinesCount)
+        {
+            wordCard = wordCards[i];
+        }
+        else
+        {
+            wordCard = wordCards[pointerPosition - wcAreaLinesCount + 1 + i];
+        }
+        content += $"{wordCard.Word} {wordCard.Translation}";
+
+        // заполняем оставшуюся часть строки пробелами
+        for (int j = 0; j < border.Length - content.Length + j - 1; j++)
+        {
+            content += " ";
+        }
+
+        content += "|";
+
+        Console.WriteLine(content);
+    }
+
+    // Рисуем нижнюю границу и выводим количество карточек
+    Console.WriteLine( $"{border} Word Cards Count: {wordCards.Count}");
+
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Edit");
+    Console.WriteLine("3 - Delete");
+    Console.WriteLine("Backspace - Back");
+}
+
+void RunTestingMenu()
+{
+    var isTestingMenuActive = true;
+    while (isTestingMenuActive)
+    {
+        UpdateTestingMenu();
+
+        var input = Console.ReadKey();
+
+        switch (input.Key)
+        {
+            case ConsoleKey.Backspace:
+                isTestingMenuActive = false;
+                break;
+            default:
+                UpdateTestingMenu();
+                break;
+        }
+    }
+}
+
+void UpdateTestingMenu()
+{
+    Console.Clear();
+
+    Console.WriteLine("IN DEVELOPMENT...");
+    Console.WriteLine();
+
+    Console.WriteLine("Backspace - Back");
 }
 
 void LoadWordCardsFromFile(string path)
@@ -80,31 +252,18 @@ void SaveWordCardsToFile(string path)
     File.WriteAllText(path, textToFile);
 }
 
-void PrintWordCards()
-{
-    var rowNumber = 1;
-    foreach (var wordCard in wordCards)
-    {
-        Console.WriteLine($"{rowNumber} {wordCard.Word} {wordCard.Translation} {wordCard.CreationDate}");
-        rowNumber++;
-    }
-}
-
 void AddNewWordCard(string word, string translation)
 {
-    wordCards.Add(new WordCard(word, translation));
+    var wordCardToAdd = new WordCard(word, translation);
+    wordCards.Add(wordCardToAdd);
 }
 
-void DeleteWordCard(string word)
+void EditWordCard(WordCard wordCard, string newWord, string newTranslation)
 {
-    foreach (var wordCard in wordCards)
-    {
-        if (wordCard.Word == word)
-        {
-            wordCards.Remove(wordCard);
-            return;
-        }
-    }
+    wordCards[wordCards.IndexOf(wordCard)] = new WordCard(newWord, newTranslation);
+}
 
-    Console.WriteLine("This word card cannot be found.");
+void DeleteWordCard(WordCard wordCardToDelete)
+{
+    wordCards.Remove(wordCardToDelete);
 }
