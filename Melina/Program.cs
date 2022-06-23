8a6e8bd4993ac8c085c326af80cd8be1770da9b1
Victor Stepin <vicstep1994@gmail.com>
2022-06-23 22:54:38 +0700

    Random Ten Test mode has been added.

--- Melina/Program.cs
+++ Melina/Program.cs
@@ -207,6 +207,9 @@ void RunTestingMenu()
             case ConsoleKey.D1:
                 RunAllCardsTest();
                 break;
+            case ConsoleKey.D2:
+                RunRandomTenTest();
+                break;
             case ConsoleKey.Backspace:
                 isTestingMenuActive = false;
                 break;
@@ -222,6 +225,7 @@ void UpdateTestingMenu()
     Console.WriteLine();
 
     Console.WriteLine("1 - All Cards Test");
+    Console.WriteLine("2 - Random Ten Test");
     Console.WriteLine("Backspace - Back");
 }
 
@@ -269,12 +273,6 @@ void DeleteWordCard(WordCard wordCardToDelete)
 void RunAllCardsTest()
 {
     var wordCardsToTest = Shuffle(wordCards);
-    //var wordCardsToTest = new List<WordCard>();
-    //for (int i = 0; i < 15; i++)
-    //{
-    //    var randomIndex = new Random().Next(0, wordCards.Count);
-    //    wordCardsToTest.Add(wordCards[randomIndex]);
-    //}
 
     var incorrectAnsweredCards = new List<WordCard>();
     var correctAnswersCount = 0;
@@ -316,6 +314,77 @@ void RunAllCardsTest()
     Console.Clear();
 
     Console.WriteLine("ALL CARDS TEST");
+    Console.WriteLine();
+
+    Console.WriteLine("RESULT");
+    Console.WriteLine();
+
+    Console.WriteLine($"Cards tested: {wordCardsToTest.Count}");
+    Console.WriteLine($"Correct: {correctAnswersCount}");
+    Console.WriteLine($"Incorrect: {incorrectAnswersCount}");
+    var percentage = (double)correctAnswersCount / wordCardsToTest.Count;
+    Console.WriteLine($"Percentage: {percentage} %");
+    Console.WriteLine();
+
+    var iwcFilePath = "incorrect_cards.txt"; //iwc - incorrect word cards
+    SaveWordCardsToFile(iwcFilePath, incorrectAnsweredCards);
+    Console.WriteLine($"Incorrect word cards saved to {iwcFilePath}");
+    Console.WriteLine();
+
+    Console.WriteLine("Press any key to return testing menu...");
+    Console.ReadKey();
+}
+
+void RunRandomTenTest()
+{
+    var shuffledOriginal = Shuffle(wordCards);
+    
+    var wordCardsToTest = new List<WordCard>();
+    for (int i = 0; i < 10; i++)
+    {
+        wordCardsToTest.Add(shuffledOriginal[i]);
+    }
+
+    var incorrectAnsweredCards = new List<WordCard>();
+    var correctAnswersCount = 0;
+    var incorrectAnswersCount = 0;
+
+
+    for (int i = 0; i < wordCardsToTest.Count; i++)
+    {
+        Console.Clear();
+
+        Console.WriteLine("RANDOM TEN TEST");
+        Console.WriteLine();
+
+        Console.WriteLine($"{wordCardsToTest.Count - i} words left");
+        Console.WriteLine();
+
+        Console.WriteLine($"Word: {wordCardsToTest[i].Word}");
+
+        Console.WriteLine("Press Enter to show translation");
+        Console.ReadKey();
+
+        Console.WriteLine($"Translation: {wordCardsToTest[i].Translation}");
+
+        Console.Write("Is your answer right? (Y/N): ");
+        var answerInput = Console.ReadKey();
+
+        switch (answerInput.Key)
+        {
+            case ConsoleKey.Y:
+                correctAnswersCount++;
+                break;
+            default:
+                incorrectAnsweredCards.Add(wordCardsToTest[i]);
+                incorrectAnswersCount++;
+                break;
+        }
+    }
+
+    Console.Clear();
+
+    Console.WriteLine("RANDOM TEN TEST");
     Console.WriteLine();
 
     Console.WriteLine("RESULT");
