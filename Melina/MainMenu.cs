d457a76cfb6a2c8571c0b6e5e16551a1d0951a49
Victor Stepin <vicstep1994@gmail.com>
2022-07-09 23:03:01 +0700

    Main menu functionality moved to new class"MainMenu".

---
+++ Melina/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿namespace Melina
+{
+    internal class MainMenu
+    {
+        private WordCardsList wordCards;
+        private bool isMenuActive;
+
+        public MainMenu(WordCardsList wordCardList)
+        {
+            wordCards = wordCardList;
+            isMenuActive = true;
+        }
+
+        public void Run()
+        {
+            while (isMenuActive)
+            {
+                Update();
+
+                var input = Console.ReadKey();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        new WordCardsManagementMenu(wordCards).Run();
+                        break;
+                    case ConsoleKey.D2:
+                        new TestingMenu(wordCards).Run();
+                        break;
+                    case ConsoleKey.X:
+                        Console.Clear();
+                        isMenuActive = false;
+                        wordCards.SaveWordCardsToFile("words.txt");
+                        break;
+                    default:
+                        Update();
+                        break;
+                }
+            }
+        }
+
+        private void Update()
+        {
+            Console.Clear();
+
+            Console.WriteLine("MELINA");
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Word Cards Management");
+            Console.WriteLine("2 - Testing");
+            Console.WriteLine("X - Exit");
+        }
+    }
+}
--- Melina/Program.cs
+++ Melina/Program.cs
@@ -1,41 +1,4 @@
 ﻿using Melina;
 
 WordCardsList wordCards = new WordCardsList("words.txt");
-
-var isProgramRunning = true;
-while (isProgramRunning)
-{
-    UpdateMainMenu();
-    
-    var input = Console.ReadKey();
-
-    switch (input.Key)
-    {
-        case ConsoleKey.D1:
-            new WordCardsManagementMenu(wordCards).Run();
-            break;
-        case ConsoleKey.D2:
-            new TestingMenu(wordCards).Run();
-            break;
-        case ConsoleKey.X:
-            Console.Clear();
-            isProgramRunning = false;
-            wordCards.SaveWordCardsToFile("words.txt");
-            break;
-        default:
-            UpdateMainMenu();
-            break;
-    }
-}
-
-void UpdateMainMenu()
-{
-    Console.Clear();
-
-    Console.WriteLine("MELINA");
-    Console.WriteLine();
-
-    Console.WriteLine("1 - Word Cards Management");
-    Console.WriteLine("2 - Testing");
-    Console.WriteLine("X - Exit");
-}
+new MainMenu(wordCards).Run();
